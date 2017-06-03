539c70bdb081557bbc37690f7e7b74fb4604357f
Yuki Iwanaga <yuki@creasty.com>
2017-06-04 01:59:53 +0900

    Update

--- Casks/keyboard.rb
+++ Casks/keyboard.rb
@@ -1,6 +1,6 @@
 cask 'keyboard' do
-  version '0.1.1'
-  sha256 'e030f15d5a18900a4cebd6bc8ff946be3cbe282bf9c755d0bf0eba883ef24ad7'
+  version '0.1.2'
+  sha256 '23736ffe6b4dbf08e95d592ba04c15474137bf5eb4761df4f145bc21548ca872'
 
   url "https://github.com/creasty/Keyboard/releases/download/v#{version}/keyboard-sierra.zip"
   name 'Keyboard'
