3e0f31d52cc3b9f56e42741e57145652edeeb71c
Yuki Iwanaga <yuki@creasty.com>
2017-06-04 01:47:48 +0900

    Update keyboard

--- Casks/keyboard.rb
+++ Casks/keyboard.rb
@@ -1,8 +1,8 @@
 cask 'keyboard' do
-  version '0.1.0'
-  sha256 'fb39fc5413aedbeeaea8e5f4be8c70e949422ad1a83565ea687bded0ad37b7d4'
+  version '0.1.1'
+  sha256 'e030f15d5a18900a4cebd6bc8ff946be3cbe282bf9c755d0bf0eba883ef24ad7'
 
-  url "https://github.com/creasty/Keyboard/releases/download/v#{version}/Keyboard-#{version}-sierra.zip"
+  url "https://github.com/creasty/Keyboard/releases/download/v#{version}/keyboard-sierra.zip"
   name 'Keyboard'
   homepage 'https://github.com/creasty/Keyboard'
 
