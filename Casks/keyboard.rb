cf44d0a8635ba4f370c94e021f829ca884c25b04
Yuki Iwanaga <yuki@creasty.com>
2019-01-26 22:57:52 +0900

    Add appcast

--- Casks/keyboard.rb
+++ Casks/keyboard.rb
@@ -3,6 +3,7 @@ cask 'keyboard' do
   sha256 '40dbb4ae652b2d19393020d5e7df1c0b2c766c4a60f65beb5d438ae9ffa8de8a'
 
   url "https://github.com/creasty/Keyboard/releases/download/v#{version}/keyboard.zip"
+  appcast 'https://github.com/creasty/Keyboard/releases.atom'
   name 'Keyboard'
   homepage 'https://github.com/creasty/Keyboard'
 
