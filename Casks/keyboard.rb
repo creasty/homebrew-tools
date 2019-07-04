64a656d07fada1ea08efb1969dfcebd5835cf8d4
Yuki Iwanaga <yuki@creasty.com>
2019-07-04 11:55:21 +0900

    Bump keyboard up to 1.2.3

--- Casks/keyboard.rb
+++ Casks/keyboard.rb
@@ -1,6 +1,6 @@
 cask 'keyboard' do
-  version '1.2.2'
-  sha256 '7540a7673db28784a95e23d229a829ec7a3cb2629e1917f6c3bc091e6c86f39f'
+  version '1.2.3'
+  sha256 '154b7f8d5a563c7e1cf681cc30406d9a0bcf229d384a044d2489badc5f1ae20d'
 
   url "https://github.com/creasty/Keyboard/releases/download/v#{version}/keyboard.zip"
   appcast 'https://github.com/creasty/Keyboard/releases.atom'
