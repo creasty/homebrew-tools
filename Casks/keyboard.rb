28d0579ff7930c59ae6eeae7472161e27151ce28
Yuki Iwanaga <yuki@creasty.com>
2019-07-30 11:40:28 +0900

    Update keyboard to v1.2.5

--- Casks/keyboard.rb
+++ Casks/keyboard.rb
@@ -1,6 +1,6 @@
 cask 'keyboard' do
-  version '1.2.4'
-  sha256 '053026729f4625a6fdb047606c30277752369c340a95de2dff0c3763e9b11ade'
+  version '1.2.5'
+  sha256 'fa3245b34506dadc70f415dd497d2ad32e8b17b6bb6537f9fe4f7969a6bf084f'
 
   url "https://github.com/creasty/Keyboard/releases/download/v#{version}/keyboard.zip"
   appcast 'https://github.com/creasty/Keyboard/releases.atom'
