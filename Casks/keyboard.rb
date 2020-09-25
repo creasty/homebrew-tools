f3f43ba2263aee29d86319389405b279634f3e0a
Yuki Iwanaga <yuki@creasty.com>
2020-09-25 12:09:43 +0900

    Update keyboard to v1.3.2

--- Casks/keyboard.rb
+++ Casks/keyboard.rb
@@ -1,6 +1,6 @@
 cask 'keyboard' do
-  version '1.3.1'
-  sha256 'ee0107a40f89d1629f8fa20043b2f718be503fea8e5c24e089f6201113a12961'
+  version '1.3.2'
+  sha256 'de9e605b67d339ab2fd74ab13672399f7072e4eb2c47b9589cf3459d4a64be16'
 
   url "https://github.com/creasty/Keyboard/releases/download/v#{version}/keyboard.zip"
   appcast 'https://github.com/creasty/Keyboard/releases.atom'
