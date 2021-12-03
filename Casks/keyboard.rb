c76ef8ab3ff9b195e5e07c1bfe899f0dfc682386
Yuki Iwanaga <yuki@creasty.com>
2021-12-03 21:36:01 +0900

    Upgrade keyboard

--- Casks/keyboard.rb
+++ Casks/keyboard.rb
@@ -1,6 +1,6 @@
 cask 'keyboard' do
-  version '1.3.4'
-  sha256 'b11bdd160136becd63da22429f8bc3e29ef352bb488129794e2d09edd9966ea1'
+  version '1.4.0'
+  sha256 'd520cfd5bdf6bdb072bf3c4075f8e11c4dc07548860d66daa98e3c7488351601'
 
   url "https://github.com/creasty/Keyboard/releases/download/v#{version}/keyboard.zip"
   appcast 'https://github.com/creasty/Keyboard/releases.atom'
